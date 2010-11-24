fc635a59931cf34806c3013f19fee17161f603c7
Thomas Altmeyer <T.Altmeyer1@gmx.de>
2010-11-24 19:42:19 +0100

    Login implementiert

--- src/java/MainServlet.java
+++ src/java/MainServlet.java
@@ -4,13 +4,11 @@
  */
 
 import db.DBConnector;
-import exceptions.MySQLException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,55 +35,75 @@ public class MainServlet extends HttpServlet {
             HttpSession seas = request.getSession();
             seas.setAttribute("user", request.getParameter("user"));
             seas.setAttribute("password", request.getParameter("password"));
-            Connection c = DBConnector.getConnection();
-            System.out.println(c.getMetaData() + " " + seas.getAttribute("user"));
-            System.out.println(c.getMetaData() + " " + seas.getAttribute("password"));
 
-            out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
-            out.write("<html>");
-            out.write("<head>");
-            out.write("<meta http-equiv=\"Content - Type\" content=\"text / html;charset = iso - 8859 - 1\">");
-            out.write("<title>Untitled Document</title>");
-            out.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"start.css\">");
-            out.write("</head>");
-            out.write("<body>");
-            out.write("<div id=\"frame\">");
-            out.write("<div id=\"title\">");
-            out.write("Title des Projektes");
-            out.write("</div>");
-            out.write("<div class=\"menuTopButton\">");
-            out.write("<a href=\"#\">&Uuml;berblick</a>");
-            out.write("</div>");
-            out.write("<div class=\"menuTopButton\">");
-            out.write("<a href=\"#\">Aufgaben</a>");
-            out.write("</div>");
-            out.write("<div class=\"menuTopButton\">");
-            out.write("<a href=\"#\">Zeiten</a>");
-            out.write("</div>");
-            out.write("<div class=\"menuTopButton\">");
-            out.write("<a href=\"#\">Profil</a>");
-            out.write("</div>");
-            out.write("<div class=\"menuTopButton\">");
-            out.write("<a href=\"#\">Logout</a>");
-            out.write("</div>");
-            out.write("<div style=\"clear:both;\">");
-            out.write("</div>");
-            out.write("<div id=\"Content\">");
-            out.write("Und hier kommt der Inhalt.");
-            out.write("</div>");
-            out.write("<div>");
-            out.write("</body>");
-            out.write("</html>");
+            String user = seas.getAttribute("user").toString();
+            String password = seas.getAttribute("password").toString();
+
+            if (checkLogin(user, password)) {
+                out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
+                out.write("<html>");
+                out.write("<head>");
+                out.write("<meta http-equiv=\"Content - Type\" content=\"text / html;charset = iso - 8859 - 1\">");
+                out.write("<title>Untitled Document</title>");
+                out.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"start.css\">");
+                out.write("</head>");
+                out.write("<body>");
+                out.write("<div id=\"frame\">");
+                out.write("<div id=\"title\">");
+                out.write("Title des Projektes");
+                out.write("</div>");
+                out.write("<div class=\"menuTopButton\">");
+                out.write("<a href=\"#\">&Uuml;berblick</a>");
+                out.write("</div>");
+                out.write("<div class=\"menuTopButton\">");
+                out.write("<a href=\"#\">Aufgaben</a>");
+                out.write("</div>");
+                out.write("<div class=\"menuTopButton\">");
+                out.write("<a href=\"#\">Zeiten</a>");
+                out.write("</div>");
+                out.write("<div class=\"menuTopButton\">");
+                out.write("<a href=\"#\">Profil</a>");
+                out.write("</div>");
+                out.write("<div class=\"menuTopButton\">");
+                out.write("<a href=\"#\">Logout</a>");
+                out.write("</div>");
+                out.write("<div style=\"clear:both;\">");
+                out.write("</div>");
+                out.write("<div id=\"Content\">");
+                out.write("Und hier kommt der Inhalt.");
+                out.write("</div>");
+                out.write("<div>");
+                out.write("</body>");
+                out.write("</html>");
+            } else {
+                out.write("<a href='Login.html'>Falscher Benutzername oder falsches Passwort eingegeben!</a>");
+            }
 
 
-            seas.invalidate(); //Logout
-        } catch (SQLException ex) {
-            Logger.getLogger(MainServlet.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (MySQLException ex) {
-            Logger.getLogger(MainServlet.class.getName()).log(Level.SEVERE, null, ex);
+
+
         } finally {
             out.close();
         }
+    }
+
+    private boolean checkLogin(String user, String password) {
+        try {
+            Connection c = DBConnector.getConnection();
+            PreparedStatement ps = c.prepareStatement("SELECT password FROM User WHERE LOWER(email)=?");
+            user = user.toLowerCase();
+            ps.setString(1, user);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            if (rs.getString(1).equals(password)) {
+                ps.close();
+                return true;
+            }
+            ps.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return false;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
